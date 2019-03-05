99683a37d29f32ee289ae0250060eee3294eb3fb
Oscar F <oscarffu@gmail.com>
2019-03-04 21:13:29 -0500

    Estructura y Get, post, patch y delete

--- tareas.rb
+++ tareas.rb
@@ -2,10 +2,21 @@ require 'sinatra'
 require 'make_todo'
 
 get '/' do
+  @tareas = Tarea.all
   erb :index
 end
 
 post '/create' do
-  Tarea.create(params[:name])
+  Tarea.create("#{params[:name]}")
 	redirect '/'
+end
+
+patch '/update/:id' do
+  Tarea.update(params[:id])
+  redirect '/'
+end
+
+delete '/delete/:id' do
+  Tarea.destroy(params[:id])
+  redirect '/'
 end
